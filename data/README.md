24477f9decaa3ee67bbbc05560794d435ec7c37f
Engelman David <daengelm@ulb.ac.be>
2019-02-16 12:25:05 +0100

    added a python script to translate words from a csv and 4 datasets: animals, clothes, food, weather

---
+++ data/README.md
@@ -0,0 +1,9 @@
+**Requirements**
+
+sudo pip3 install googletrans
+
+**Usage**
+
+python3 translate.py weather.py
+
+will create a new file weather_en_fr.csv with the tranlations of all the words 
---
+++ data/translate.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from googletrans import Translator
+from sys import argv
+
+
+def translate_data(file, src_language, dest_language):
+    """Translates all the words from a csv to the dest_language and put the translation in a new column"""
+
+    #load the csv file
+    df = pd.read_csv(file, index_col=None, header=None)
+    translations = []
+    translator = Translator()
+    #iterate on all the words to translate them
+    for index, row in df.iterrows():
+        word = row[0]
+        translation = translator.translate(word, src=src_language, dest=dest_language).text
+        translations.append(translation)
+
+    df[1] = translations
+
+    file_with_transl = file.split(".")[0] + "_" + src_language + "_" + dest_language + ".csv"
+
+    df.to_csv(file_with_transl, index=None, header=None)
+
+
+if __name__ == '__main__':
+
+    #translate all the files given in argument
+    files = argv[1:]
+    for file in files:
+        translate_data(file, "en", "fr")
+
+
+
+
+
