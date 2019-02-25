5c81c3aba01c4c1df900d09a989eccb919b777e7
jeroen <jeroen.van.soest@vub.be>
2019-02-25 17:54:26 +0100

    working ajax call in ui, needs checking

--- app/src/app/register/register.page.ts
+++ app/src/app/register/register.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router,ActivatedRoute,NavigationExtras } from '@angular/router';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-register',
@@ -8,31 +10,38 @@ import { Router,ActivatedRoute,NavigationExtras } from '@angular/router';
 })
 export class RegisterPage implements OnInit {
 
-  firstName_V:string="";
-  lastName_V:string="";
-  email_V:string="";
-  constructor(private router: Router){
+  firstName_V:string;
+  lastName_V:string;
+  email_V:string;
+  data:any;
+  resp:string;
+  constructor(private router: Router,private http: HttpClient){
 
   }
 
   ngOnInit() {
   }
 
-  nextClick()
+  submitClick()
   {
     if (this.firstName_V.length > 0 && this.email_V.length > 0 && this.lastName_V.length > 0)
     {
-    alert("Here we should save the personal data in the server");
-    ///////
-    ///////
-    this.router.navigate(['preferences']);
+    this.http.post('http://localhost:5000/lolo/api/v1.0/user/register', {
+    data: {
+        user: {
+            name : this.firstName_V,
+            email : this.email_V
+        }
+    }}).subscribe(response => {
+    this.data = response;
+    console.log(this.data);
+    console.log('response :'+response);
+    });
     }
     else
-    {
      alert("Fill in the blanks, please");
-    }
-
   }
+
   goBack(){
     this.router.navigate(['home']);
   }
