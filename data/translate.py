c7b5e0da6ff0c637f1a3d1ae6bfe4894562aa03d
Engelman David <daengelm@ulb.ac.be>
2019-03-16 11:36:10 +0100

    update the translate script

--- data/translate.py
+++ data/translate.py
@@ -3,22 +3,26 @@ from googletrans import Translator
 from sys import argv
 
 
-def translate_data(file, src_language, dest_language):
-    """Translates all the words from a csv to the dest_language and put the translation in a new column"""
+LANGUAGES_TO_TRANSLATE = ["nl", "de", "es"]
+
+def translate_data(file):
+    """Translates all the words from a csv in a new column """
 
     #load the csv file
-    df = pd.read_csv(file, index_col=None, header=None)
-    translations = []
+    df = pd.read_csv(file, index_col=None, header=0)
     translator = Translator()
-    #iterate on all the words to translate them
-    for index, row in df.iterrows():
-        word = row[0]
-        translation = translator.translate(word, src=src_language, dest=dest_language).text
-        translations.append(translation)
+    for lang in LANGUAGES_TO_TRANSLATE:
+        translations = []
+        #iterate on all the words to translate them
+        for index, row in df.iterrows():
+            word = row[0]
+            translation = translator.translate(word, src="en", dest=lang).text
+            translations.append(translation)
 
-    df[df.shape[1]] = translations
+        df[lang] = translations
+    print(df)
 
-    df.to_csv(file, index=None, header=None)
+    df.to_csv(file, index=None, header=0)
 
 
 if __name__ == '__main__':
@@ -26,7 +30,8 @@ if __name__ == '__main__':
     #translate all the files given in argument
     files = argv[1:]
     for file in files:
-        translate_data(file, "en", "fr")
+        translate_data(file)
+
 
 
 
