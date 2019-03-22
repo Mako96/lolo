9195db7184de8023e4f8c09c4f417cd77aac02d3
Engelman David <daengelm@ulb.ac.be>
2019-03-22 10:39:12 +0100

    added some sentences

--- data/insert_from_json.py
+++ data/insert_from_json.py
@@ -7,13 +7,23 @@ client = MongoClient(port=27017)
 db = client.loloDB
 voc_collection = db["vocabulary"]
 
-voc_collection.remove({})
 
-for file in ["data2.json", "data3.json", "data4.json", "data5.json"]:
-    with open(file) as f:
-        data = json.load(f)
+def check_duplicate():
+    l = []
+    for word in voc_collection.find():
+        if word in l:
+            print(word)
+        l.append(word)
 
-        data = data["data"]
-        for word in data:
-            voc_collection.insert(word)
+# voc_collection.remove({})
+#
+# for file in ["data.json", "data2.json", "data3.json"]:
+#     with open(file) as f:
+#         data = json.load(f)
+#
+#         data = data["data"]
+#         for word in data:
+#             voc_collection.insert(word)
 
+
+check_duplicate()
