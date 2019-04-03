b5c2e32588fa6a1fe707aaa64173ec663f9e7c88
Jeroen Van Soest <jeroen.van.soest@vub.be>
2019-04-03 16:17:12 +0200

    added the learning language and some text to statistic page

--- app/src/app/statistics/statistics.page.ts
+++ app/src/app/statistics/statistics.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {LoloUserProviderService} from '../lolo-user-provider.service';
+import { Router,NavigationExtras, ActivatedRoute } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 
 
@@ -10,9 +11,10 @@ import { ToastController } from '@ionic/angular';
 })
 export class StatisticsPage implements OnInit {
 
-
+  public learningLang;
+  public langs = {"fr":"French", "de":"German","swe":"Swedish"}
   public completedWords = {};
-  constructor(private userProvider: LoloUserProviderService) { }
+  constructor(private userProvider: LoloUserProviderService,private router: Router) { }
 
   getUserStatistics() {
         var _self = this;
@@ -29,6 +31,27 @@ export class StatisticsPage implements OnInit {
 
   ngOnInit() {
     this.getUserStatistics()
+    this.getUserLanguage()
+  }
+
+  getUserLanguage() {
+      var _self = this;
+      var cbError = function (error) {
+          alert(error.message);
+          _self.learningLang = "fr" //if it fails the default language is french
+      };
+      var cbSucces = function (data) {
+          console.log(data);
+          _self.learningLang = data.lang
+      };
+      this.userProvider.getUserLanguage(cbSucces, cbError);
+  }
+
+
+  goBack()
+  {
+
+    this.router.navigate(['main']);
   }
 
 
