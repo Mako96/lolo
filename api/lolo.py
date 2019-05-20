4812c45e0d85a65cd9c8bad39cdf3c0c2be1b01e
thomas.desire.cloostermans-Huwaert <thomas.desire.cloostermans-huwaert@vub.be>
2019-05-20 22:04:24 +0200

    adding lolo.py which is used to train the neural network

---
+++ api/lolo.py
@@ -0,0 +1,33 @@
+from pandas import read_csv
+from sklearn.datasets import make_regression
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.optimizers import SGD
+from matplotlib import pyplot
+# load regression dataset
+dataset = read_csv('dataset.csv', header=0, infer_datetime_format=True)
+df1, df2 = dataset[['L', 'P']], dataset[['D']]
+X, y = df1.values, df2.values
+y = y.reshape((71,))
+# split into train and test
+n_train = 50
+trainX, testX = X[:n_train, :], X[n_train:, :]
+trainy, testy = y[:n_train], y[n_train:]
+# define model
+model = Sequential()
+model.add(Dense(25, input_dim=2, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(1, activation='linear'))
+# compile model
+model.compile(loss='mean_squared_error', optimizer=SGD(lr=0.01, momentum=0.9))
+# fit model
+history = model.fit(trainX, trainy, validation_data=(testX, testy), epochs=100, verbose=0)
+# evaluate the model
+train_mse = model.evaluate(trainX, trainy, verbose=0)
+test_mse = model.evaluate(testX, testy, verbose=0)
+print('Train: %.3f, Test: %.3f' % (train_mse, test_mse))
+# plot loss during training
+pyplot.title('Mean Squared Error')
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
